1264866b88ebd68fbbad6b9b2b3c6997c05164b2
saad <fouadr@hotmail.com>
2023-09-23 12:22:23 -0400

    version 11

--- assemble.py
+++ assemble.py
@@ -1,6 +1,6 @@
 #!/Users/fouad/AppData/Local/Programs/Python
 import os, sys
-print("version 5")
+print("version 11")
 
 # these three dictionaries store the translations of the 3 parts
 # of a c-instruction
